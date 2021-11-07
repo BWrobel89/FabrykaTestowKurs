fa50aca1a1bd35afcccc76539fc113ccb732e8c6
Basia <b.konieczko89@gmail.com>
2021-11-07 11:32:08 +0100

    requests library lesson

---
+++ scripts/restAPI.py
@@ -0,0 +1,19 @@
+# CREATE -> POST
+# READ -> GET
+# UPDATE - > PUT
+# DELETE -> DELETE
+
+import requests
+
+# post = requests.post('http://httpbin.org/post')
+# put = requests.put('http://httpbin.org/put')
+# delete = requests.delete('http://httpbin.org/delete')
+#
+# print(delete)
+
+parameters = {'key1':'value1', 'key2':'value2'}
+
+r = requests.get('https://fabrykatestow.pl', params=parameters)
+print(r.url)
+print(r.text)
+print(r.encoding)
