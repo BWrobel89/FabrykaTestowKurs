eda0d9c247c62e74129b70f2313b4580aa4eb19c
Basia <b.konieczko89@gmail.com>
2022-07-01 12:29:47 +0200

    update chromedriver and test

--- scripts/test1.py
+++ scripts/test1.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+my_service = Service('C:/Users/48721/Desktop/KURS PYTHON SELENIUM/chromedriver.exe')
+driver = webdriver.Chrome(service=my_service)
 
 driver.get('https://fabrykatestow.pl')
 driver.close()
